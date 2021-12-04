413e8bf3194eabf446d578b8e47a6910249a8295
Jay Heo <heojay0309@gmail.com>
2021-12-04 11:42:06 -0800

    finalized boilerplate 12/4

---
+++ client/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import HomeContainer from './containers/HomeContainer.jsx';
+// import Login from './components/Login.jsx';
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>hi</h1>
+        {/* <HomeContainer/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+// import store from './store';
+
+// So that webpack can bundle styles
+import styles from './styles.scss';
+
+render(
+  // <Provider >
+    <App />
+  // </Provider>,
+  , document.getElementById('root')
+);
